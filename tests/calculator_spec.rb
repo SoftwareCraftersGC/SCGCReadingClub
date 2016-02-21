ad3acb17e71f3dd9cf046dc77eaa44071b324b03
Ronny Ancorini <Ronnygx@gmail.com>
2016-02-21 01:03:32 +0000

    To do list

--- tests/calculator_spec.rb
+++
@@ -1,6 +0,0 @@
-describe 'Sum' do
-  it '2 + 2 = 4' do
-    expect(2 + 2).to eq(4)
-  end
-
-end
---
+++ tests/reading_list_actions_spec.rb
@@ -0,0 +1,21 @@
+describe GetReadingListAction do
+
+  it 'should get the reading list for a existing user' do
+  end
+
+  it 'should get that list ordered alphabetically' do
+  end
+end
+
+describe AddBookToReadingListAction do
+  it "should add a book to te user's list" do
+  end
+
+  it "should not add two books with same author and title" do
+  end
+end
+
+describe DeleteBookToReadingListAction do
+  it "should delete a book from the reading list of a user" do
+  end
+end
